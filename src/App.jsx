7e887304f940efdf421f9871c3c08ec8eccb5c4a
Unumongmarmahub <unumongmarma1998@gmail.com>
2025-01-28 19:08:07 +0600

    install vite react daisyui uapdeted version

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-8xl">Hi, I am form react router</h1>
+      <h1 className="text-2xl bg-amber-400 text-center">Hi, I am form react router</h1>
     </>
   );
 }
